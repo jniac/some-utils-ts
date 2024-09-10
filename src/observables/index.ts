f44c5cf416668af8a1dc82b1f57ba64daf01ba62
jniac <joseph.merdrignac@gmail.com>
2024-09-10 20:22:36 +0200

    adding foo.ts for any foo/index.ts entry (avoiding exports trickeries in package.json)

---
+++ src/observables/index.ts
@@ -0,0 +1,11 @@
+export type {
+  Callback as ObservableCallback,
+  ConstructorOptions as ObservableConstructorOptions,
+  ObservableCore,
+  DerivativeCallback as ObservableDerivativeCallback,
+  OnChangeOptions as ObservableOnChangeOptions
+} from './observable'
+
+export { Observable } from './observable'
+export { ObservableNumber } from './observable-number'
+
