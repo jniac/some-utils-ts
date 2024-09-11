9727ae16adcc6dbde48786d70aae48e2ce009e9f
jniac <joseph.merdrignac@gmail.com>
2024-09-11 13:28:19 +0200

    glsl: better easings (power up to 10, "easeOutIn")

--- src/glsl/easings.ts
+++ src/glsl/easings.ts
@@ -2,25 +2,13 @@ import { glsl_basic } from './basic'
 
 export const easingsNames = [
   'linear',
-  'easeIn1',
-  'easeIn2',
-  'easeIn3',
-  'easeIn4',
-  'easeIn5',
-  'easeIn6',
-  'easeOut1',
-  'easeOut2',
-  'easeOut3',
-  'easeOut4',
-  'easeOut5',
-  'easeOut6',
-  'easeInout1',
-  'easeInout2',
-  'easeInout3',
-  'easeInout4',
-  'easeInout5',
-  'easeInout6',
 ]
+for (let i = 1; i <= 10; i++) {
+  easingsNames.push(`easeIn${i}`)
+  easingsNames.push(`easeOut${i}`)
+  easingsNames.push(`easeInOut${i}`)
+  easingsNames.push(`easeOutIn${i}`)
+}
 
 export const glsl_easings = /* glsl */`
 #ifndef GLSL_EASING
@@ -28,90 +16,91 @@ export const glsl_easings = /* glsl */`
 
 ${glsl_basic}
 
+float easePow1(float x) {
+  return x;
+}
+
+float easePow2(float x) {
+  return x * x;
+}
+
+float easePow3(float x) {
+  return x * x * x;
+}
+
+float easePow4(float x) {
+  x *= x;
+  return x *= x;
+}
+
+float easePow5(float x) {
+  float x0 = x;
+  x *= x;
+  x *= x;
+  return x0 * x;
+}
+
+float easePow6(float x) {
+  x *= x * x;
+  return x *= x;
+}
+
+float easePow7(float x) {
+  float x0 = x;
+  x *= x * x;
+  x *= x;
+  return x0 * x;
+}
+
+float easePow8(float x) {
+  x *= x;
+  x *= x;
+  return x *= x;
+}
+
+float easePow9(float x) {
+  x *= x * x;
+  return x *= x * x;
+}
+
+float easePow10(float x) {
+  float x0 = x;
+  x *= x * x;
+  x *= x * x;
+  return x0 * x;
+}
+
 float linear(float x) {
   return clamp01(x);
 }
 
-float easeIn1 (float x) {
-  x = clamp01(x);
-  return x;
+${Array.from({ length: 10 }, (_, i) => {
+  const p = i + 1
+  return /* glsl */`
+
+float easeIn${p} (float x) {
+  return easePow${p}(clamp01(x));
 }
-float easeIn2 (float x) {
-  x = clamp01(x);
-  return x * x;
+float easeOut${p} (float x) {
+  return 1.0 - easePow${p}(clamp01(1.0 - x));
 }
-float easeIn3 (float x) {
-  x = clamp01(x);
-  return x * x * x;
+float easeInOut${p} (float x) {
+  return x < 0.5 
+    ? 0.5 * easePow${p}(2.0 * x) 
+    : 1.0 - 0.5 * easePow${p}(2.0 * (1.0 - x));
 }
-float easeIn4 (float x) {
-  x = clamp01(x);
-  return x * x * x * x;
-}
-float easeIn5 (float x) {
-  x = clamp01(x);
-  return x * x * x * x * x;
-}
-float easeIn6 (float x) {
-  x = clamp01(x);
-  x *= x * x;
-  return x * x;
+float easeOutIn${p} (float x) {
+  return x < 0.5
+    ? 0.5 * (1.0 - easePow${p}(1.0 - x * 2.0))
+    : 1.0 - 0.5 * (1.0 - easePow${p}(2.0 * x - 1.0));
 }
 
-float easeOut1 (float x) {
-  x = clamp01(x);
-  return x;
-}
-float easeOut2 (float x) {
-  x = clamp01(x);
-  return 1.0 - (x = 1.0 - x) * x;
-}
-float easeOut3 (float x) {
-  x = clamp01(x);
-  return 1.0 - (x = 1.0 - x) * x * x;
-}
-float easeOut4 (float x) {
-  x = clamp01(x);
-  return 1.0 - (x = 1.0 - x) * x * x * x;
-}
-float easeOut5 (float x) {
-  x = clamp01(x);
-  return 1.0 - (x = 1.0 - x) * x * x * x * x;
-}
-float easeOut6 (float x) {
-  x = clamp01(x);
-  x = 1.0 - x;
-  x *= x * x;
-  return 1.0 - x * x;
-}
+`.trim()
+}).join('\n\n')}
 
-float easeInout1 (float x) {
-  x = clamp01(x);
-  return x;
-}
-float easeInout2 (float x) {
-  x = clamp01(x);
-  return x < 0.5 ? 2.0 * x * x : 1.0 - 2.0 * (x = 1.0 - x) * x;
-}
-float easeInout3 (float x) {
-  x = clamp01(x);
-  return x < 0.5 ? 4.0 * x * x * x : 1.0 - 4.0 * (x = 1.0 - x) * x * x;
-}
-float easeInout4 (float x) {
-  x = clamp01(x);
-  return x < 0.5 ? 8.0 * x * x * x * x : 1.0 - 8.0 * (x = 1.0 - x) * x * x * x;
-}
-float easeInout5 (float x) {
-  x = clamp01(x);
-  return x < 0.5 ? 16.0 * x * x * x * x * x : 1.0 - 16.0 * (x = 1.0 - x) * x * x * x * x;
-}
-float easeInout6 (float x) {
-  x = clamp01(x);
-  return x < 0.5 ? 32.0 * x * x * x * x * x * x : 1.0 - 32.0 * (x = 1.0 - x) * x * x * x * x * x;
-}
 
 // https://www.desmos.com/calculator/mqou4lf9zc?lang=fr
-float easeInout(float x, float p, float i) {
+float easeInOut(float x, float p, float i) {
   return  x <= 0.0 ? 0.0 :
           x >= 1.0 ? 1.0 :
           x <= i ? 1.0 / pow(i, p - 1.0) * pow(x, p) :
