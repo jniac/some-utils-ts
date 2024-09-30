c4757f35cf3ceb36e49c2a0152df6112c4620388
jniac <joseph.merdrignac@gmail.com>
2024-09-30 12:54:11 +0200

    object/misc: omit() for... omitting props of a source

--- src/object/misc.ts
+++ src/object/misc.ts
@@ -2,3 +2,7 @@
 export function withoutUndefined<T extends Record<string, any>>(object: T): Partial<T> {
   return Object.fromEntries(Object.entries(object).filter(([, value]) => value !== undefined)) as Partial<T>
 }
+
+export function omit<T extends Record<string, any>, K extends keyof T>(object: T, ...keys: K[]): Omit<T, K> {
+  return Object.fromEntries(Object.entries(object).filter(([key]) => !keys.includes(key as K))) as Omit<T, K>
+}
