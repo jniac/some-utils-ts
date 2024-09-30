67886df0975b11292dfc01d74b72e9845fac64c2
jniac <joseph.merdrignac@gmail.com>
2024-09-30 13:06:40 +0200

    (rfctr, impr.) animation: big update, cf commit & readme

--- src/animation/animation.ts
+++ src/animation/animation.ts
@@ -2,6 +2,8 @@ import { MultiKeyWeakMap } from '../collection/multi-key-map'
 import { clamp, clamp01 } from '../math/basic'
 import { isObject } from '../object/common'
 import { expandObject } from '../object/expand'
+import { omit } from '../object/misc'
+import { DestroyableObject } from '../types'
 import { EasingDeclaration, easing } from './easing'
 
 type Callback = (animation: AnimationInstance) => void
@@ -29,21 +31,28 @@ class MultiValueMap<K, V> {
 const onUpdate = new MultiValueMap<number, Callback>()
 const onDestroy = new MultiValueMap<number, Callback>()
 
-const instanceWeakMap = new MultiKeyWeakMap<any, AnimationInstance>()
+const instanceMultiKeyWeakMap = new MultiKeyWeakMap<any, AnimationInstance>()
 
 let animationInstanceCounter = 0
-class AnimationInstance {
+class AnimationInstance implements DestroyableObject {
   readonly id = animationInstanceCounter++
 
   readonly duration: number
   readonly delay: number
   readonly target: any
+  readonly prerun: boolean
+  readonly autoDestroy: boolean
+
+  destroyHasBeenRequested: boolean = false
+
   unclampedTime: number
   unclampedTimeOld: number
   frame: number
   timeScale: number
 
-  // Computed user destinated values.
+  paused: boolean = false
+
+  // Computed "user-destinated" values.
   time: number = 0
   timeOld: number = 0
   progress: number = 0
@@ -53,7 +62,7 @@ class AnimationInstance {
   get delayed() { return this.unclampedTime < 0 }
   get deltaTime() { return this.time - this.timeOld }
 
-  constructor(duration: number, delay: number, timeScale: number, target: any) {
+  constructor(duration: number, delay: number, timeScale: number, target: any, autoDestroy: boolean, prerun: boolean | undefined) {
     this.duration = duration
     this.delay = delay
     this.frame = 0
@@ -61,6 +70,8 @@ class AnimationInstance {
     this.unclampedTimeOld =
       this.unclampedTime = -delay
     this.target = target
+    this.autoDestroy = autoDestroy
+    this.prerun = prerun ?? delay > 0 // prerun if delay is set.
   }
 
   onUpdate(callback: Callback): this {
@@ -81,6 +92,24 @@ class AnimationInstance {
     return this
   }
 
+  /**
+   * Request the instance to be destroyed.
+   * 
+   * NOTE: The instance is not destroyed immediately, it will be destroyed:
+   * - at the end of the current update loop (if inside an update loop)
+   * - at the end of the next update loop (if outside an update loop, callbacks will be ignored)
+   */
+  requestDestroy() {
+    this.destroyHasBeenRequested = true
+  }
+
+  /**
+   * Call internally the `requestDestroy` method. Here to implement the `DestroyableObject` interface.
+   * 
+   * NOTE: The instance is not destroyed immediately, it will be destroyed as soon as possible, callbacks will be ignored.
+   */
+  destroy = () => this.requestDestroy()
+
   // Facilitators:
   /**
    * Lerps the current progress value.
@@ -98,6 +127,56 @@ class AnimationInstance {
     const alpha = easing(ease)(this.progress)
     return from + (to - from) * alpha
   }
+
+  set({ paused, time, progress }: Partial<{
+    paused: boolean
+    time: number
+    progress: number
+  }> = {}) {
+    if (progress !== undefined && Number.isFinite(progress)) {
+      time = progress * this.duration
+    }
+
+    if (time !== undefined && Number.isFinite(time)) {
+      this.unclampedTime = time
+    }
+
+    if (paused !== undefined) {
+      this.paused = paused
+    }
+
+    return this
+  }
+
+  /**
+   * Pauses the instance.
+   * 
+   * Convenient method to set `paused` to `true`, equivalent to:
+   * ```
+   * instance.set({ paused: true })
+   * ```
+   */
+  pause(props?: number | Parameters<AnimationInstance['set']>[0]) {
+    if (typeof props === 'number') {
+      props = { time: props }
+    }
+    return this.set({ ...props, paused: true })
+  }
+
+  /**
+   * Resumes the instance.
+   * 
+   * Convenient method to set `paused` to `false`, equivalent to:
+   * ```
+   * instance.set({ paused: false })
+   * ```
+   */
+  play(props?: number | Parameters<AnimationInstance['set']>[0]) {
+    if (typeof props === 'number') {
+      props = { time: props }
+    }
+    return this.set({ ...props, paused: false })
+  }
 }
 
 let instances: AnimationInstance[] = []
@@ -111,13 +190,12 @@ const destroyInstance = (instance: AnimationInstance) => {
 }
 
 const registerInstance = <T extends AnimationInstance>(instance: T): T => {
+  ensureAnimationLoop()
   const { target } = instance
   if (target !== undefined) {
-    const existingInstance = instanceWeakMap.get(target)
-    if (existingInstance) {
-      destroyInstance(existingInstance)
-    }
-    instanceWeakMap.set(target, instance)
+    const existingInstance = instanceMultiKeyWeakMap.get(target)
+    existingInstance?.requestDestroy()
+    instanceMultiKeyWeakMap.set(target, instance)
   }
   instances.push(instance)
   return instance
@@ -127,10 +205,14 @@ const updateInstances = (deltaTime: number) => {
   for (let i = 0, max = instances.length; i < max; i++) {
     const instance = instances[i]
 
-    instance.frame++
+    // Skip destroyed instances.
+    if (instance.destroyHasBeenRequested) {
+      continue
+    }
 
-    instance.unclampedTimeOld = instance.unclampedTime
-    instance.unclampedTime += deltaTime * instance.timeScale
+    if (instance.paused === false && instance.unclampedTime < instance.duration) {
+      instance.unclampedTime += deltaTime * instance.timeScale
+    }
 
     instance.timeOld = instance.time
     instance.time = clamp(instance.unclampedTime, 0, instance.duration)
@@ -139,16 +221,20 @@ const updateInstances = (deltaTime: number) => {
       ? clamp01(instance.time / instance.duration)
       : 0 // progress is zero on infinite animation.
 
-    if (instance.unclampedTime > 0 || instance.frame === 1) {
+    const hasChanged = instance.unclampedTime >= 0 && instance.unclampedTime !== instance.unclampedTimeOld
+    if (hasChanged || (instance.frame === 0 && instance.prerun)) {
       for (const callback of onUpdate.get(instance.id)) {
         callback(instance)
+        instance.frame++
       }
     }
+
+    instance.unclampedTimeOld = instance.unclampedTime
   }
 
   const instancesToBeDestroyed = new Set<AnimationInstance>()
   instances = instances.filter(instance => {
-    if (instance.progress === 1) {
+    if (instance.destroyHasBeenRequested || (instance.autoDestroy && instance.complete)) {
       instancesToBeDestroyed.add(instance)
       return false
     }
@@ -161,7 +247,14 @@ const updateInstances = (deltaTime: number) => {
 }
 
 let loopId = -1
+let animationLoopStarted = false
+function ensureAnimationLoop() {
+  if (animationLoopStarted === false) {
+    startAnimationLoop()
+  }
+}
 function startAnimationLoop() {
+  animationLoopStarted = true
   let msOld = window.performance.now()
   const loop = (ms: number) => {
     loopId = window.requestAnimationFrame(loop)
@@ -178,47 +271,61 @@ function stopAnimationLoop() {
 // --------------[ Clear ]--------------- //
 
 function clear(target: any) {
-  const instance = instanceWeakMap.get(target)
+  const instance = instanceMultiKeyWeakMap.get(target)
   if (instance) {
-    destroyInstance(instance)
+    instance.requestDestroy()
   }
 }
 
 
 // --------------[ During ]--------------- //
-
-type DuringArg = {
+const defaultDuringArg = {
   /**
    * Duration in seconds.
    */
-  duration: number
+  duration: 1,
   /**
    * Delay in seconds.
    */
-  delay?: number
+  delay: 0,
   /**
    * Time scale.
    */
-  timeScale?: number
+  timeScale: 1,
   /**
    * Target, if an instance is associated with a target, it will be destroyed 
    * when a new instance is created for the same target.
+   * 
+   * Target can be any value or a combination of values:
+   * - `[myObject, 'myProperty']` won't conflict with `[myObject, 'myOtherProperty']`
    */
-  target?: any
+  target: <any>undefined,
+  /**
+   * If true, the instance will be destroyed when it completes.
+   * 
+   * Defaults to `true`.
+   */
+  autoDestroy: true,
+  /**
+   * If true, the instance will be updated on the first frame.
+   * 
+   * Defaults to the delay value (if any delay is set prerun will be set to true, otherwise it will be false).
+   */
+  prerun: <undefined | boolean>undefined,
 }
 
-function during(arg: DuringArg): AnimationInstance
+type DuringArg = Partial<typeof defaultDuringArg>
+
 /**
  * 
  * @param duration duration in seconds
  */
-function during(duration: number): AnimationInstance
-function during(arg: any) {
-  const [duration, delay, timeScale, target] = (typeof arg === 'number'
-    ? [arg, 0, 1, undefined]
-    : [arg.duration, arg.delay ?? 0, arg.timeScale ?? 1, arg.target]
-  ) as [number, number, number, any]
-  return registerInstance(new AnimationInstance(duration, delay, timeScale, target))
+function during(arg: DuringArg | number): AnimationInstance {
+  if (typeof arg === 'number') {
+    arg = { duration: arg }
+  }
+  const { duration, delay, timeScale, target, autoDestroy, prerun } = { ...defaultDuringArg, ...arg }
+  return registerInstance(new AnimationInstance(duration, delay, timeScale, target, autoDestroy, prerun))
 }
 
 
@@ -226,9 +333,7 @@ function during(arg: any) {
 // --------------[ Tween ]--------------- //
 
 const defaultTweenArg = {
-  duration: 1,
-  delay: 0,
-  timeScale: 1,
+  ...omit(defaultDuringArg, 'target'),
   ease: <EasingDeclaration>'inOut2',
 }
 
@@ -296,11 +401,13 @@ function tween<T extends Record<string, any>>(arg: TweenArg<T>): TweenInstance {
     delay,
     timeScale,
     ease,
+    autoDestroy,
+    prerun,
     target,
     from,
     to,
   } = { ...defaultTweenArg, ...arg }
-  const instance = registerInstance(new TweenInstance(duration, timeScale, delay, target))
+  const instance = registerInstance(new TweenInstance(duration, timeScale, delay, target, autoDestroy, prerun))
   if (from ?? to) {
     instance.add({ target, from, to })
   }
