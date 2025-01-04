a861dd4cb30285e4f798b4953e1469bd50fdb8d3
jniac <joseph.merdrignac@gmail.com>
2025-01-04 14:06:28 +0100

    (rfctr) ObservableRecord -> ObservableTree (reflects the actual structure)

--- src/observables/index.ts
+++ src/observables/index.ts
@@ -8,6 +8,6 @@ export type {
 
 export { Observable } from './observable'
 export { ObservableNumber } from './observable-number'
-export { ObservableRecord } from './observable-record'
 export { ObservableSet } from './observable-set'
+export { ObservableTree } from './observable-tree'
 
