135f26ab528ad7390fa1c7ec4c8fde149ac1ccac
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2025-02-21 16:55:47 +0100

    Update post-build.mjs (fs auto look for files, regular, index etc)

--- scripts/post-build.mjs
+++ scripts/post-build.mjs
@@ -1,26 +1,70 @@
+// https://github.com/jniac/some-utils-ts/edit/main/scripts/post-build.mjs
+
 import fs from 'fs/promises'
 import path from 'path'
 
 /**
  * Adds .js extensions to relative imports in a single file.
+ * 
+ * Features:
+ * - ignores existing .js imports
+ * - ignores .json imports
+ * - adds .js to regular files
+ * - adds /index.js to directories with an index file
+ * 
  * @param {string} filePath - The path to the file.
  */
 async function addJsExtensionsToFile(filePath) {
   const content = await fs.readFile(filePath, 'utf8')
-  const importRegex = /from\s+['"](\.\.?\/[^'"]+)['"]/g
-  const newContent = content.replace(importRegex, (match, importPath) => {
-    if (!importPath.endsWith('.js')) {
-      return `from '${importPath}.js'`
-    }
-    return match
-  })
+  const re = /from\s+['"](\.\.?\/[^'"]+)['"]/g
+  const matches = content.matchAll(re)
 
-  let count = 0
-  if (newContent !== content) {
-    await fs.writeFile(filePath, newContent, 'utf8')
-    count++
+  let replaced = false
+  let index = 0
+  const chunks = []
+  for (const m of matches) {
+    const [m0, m1] = m
+
+    if (m1.endsWith('.js'))
+      continue
+
+    if (m1.endsWith('.json'))
+      continue
+
+    chunks.push(content.slice(index, m.index))
+
+    const regularFileExists = await fs.access(path.join(path.dirname(filePath), m1 + '.js'))
+      .then(() => true)
+      .catch(() => false)
+
+    if (regularFileExists) {
+      chunks.push(m0.replace(m1, `${m1}.js`))
+    } else {
+      const indexFileExists = await fs.access(path.join(path.dirname(filePath), m1, 'index.js'))
+        .then(() => true)
+        .catch(() => false)
+
+      if (indexFileExists) {
+        chunks.push(m0.replace(m1, `${m1}/index.js`))
+      } else {
+        console.warn(`File not found: ${m1}.js or ${m1}/index.js`)
+        chunks.push(m0)
+      }
+    }
+
+    replaced = true
+
+    index = m.index + m0.length
   }
-  return count
+
+  if (replaced === false)
+    return 0
+
+  chunks.push(content.slice(index))
+  const newContent = chunks.join('')
+  await fs.writeFile(filePath, newContent, 'utf8')
+
+  return chunks.length - 1
 }
 
 /**
