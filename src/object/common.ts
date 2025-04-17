6fb7f30cc1ff2ee0c2fa847d897012e04384fd5e
jniac <joseph.merdrignac@gmail.com>
2025-04-17 15:48:32 +0200

    object: isRecord

--- src/object/common.ts
+++ src/object/common.ts
@@ -2,3 +2,9 @@
 export function isObject(value: any): value is Record<string | number | symbol, any> {
   return value !== null && typeof value === 'object'
 }
+
+export function isRecord<TKey extends string | number | symbol = string, TValue = any>(
+  value: any,
+): value is Record<TKey, TValue> {
+  return isObject(value) && Array.isArray(value) === false
+}
