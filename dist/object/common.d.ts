7b1415641cd1f113344c0617876d273e3d9d3961
jniac <joseph.merdrignac@gmail.com>
2025-04-17 15:48:32 +0200

    object: isRecord

--- dist/object/common.d.ts
+++ dist/object/common.d.ts
@@ -1,1 +1,2 @@
 export declare function isObject(value: any): value is Record<string | number | symbol, any>;
+export declare function isRecord<TKey extends string | number | symbol = string, TValue = any>(value: any): value is Record<TKey, TValue>;
