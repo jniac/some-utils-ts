34f731880c077bc51b38059e7a6d38abad53e0b3
jniac <joseph.merdrignac@gmail.com>
2025-06-03 16:14:30 +0200

    fix NaN with negative seed 😓

--- src/random/algorithm/parkmiller-c-iso.ts
+++ src/random/algorithm/parkmiller-c-iso.ts
@@ -1,19 +1,25 @@
 const MAX = 0x7fffffff
 const DEFAULT_SEED = 123456
 
-const init = (seed: number = DEFAULT_SEED) => {
-  // Trying to avoid low randomness for low seeds
-  seed = seed * 7 + Math.sqrt(seed) + Math.sin(seed) * 16087
+const init = (seedArg: number = DEFAULT_SEED) => {
+  if (Number.isNaN(seedArg))
+    throw new Error(`NaN is not a valid seed.`)
+
+  let seed = seedArg
+
+  if (Math.abs(seed) < 10)
+    seed *= 1000000
 
   seed = seed % MAX
   seed = seed < 0 ? seed + MAX : seed
-  if (seed > 1) {
+
+  if (seed > 1)
     return seed & MAX
-  }
-  if (seed === 0) {
-    return 345678
-  }
-  throw new Error('Impossible.')
+
+  if (seed === 0)
+    return DEFAULT_SEED
+
+  throw new Error(`Impossible. ${seed}`)
 }
 
 const next = (state: number) => {
