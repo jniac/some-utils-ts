dfe2f09b0435336a4be57fba70e049d6e2f2cc0e
jniac <joseph.merdrignac@gmail.com>
2025-06-05 13:54:42 +0200

    stegu noise: + snoiseFast4D(), fnoiseFast4D()

--- src/glsl/stegu-snoise.ts
+++ src/glsl/stegu-snoise.ts
@@ -21,6 +21,10 @@ float mod289(float x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0; 
 }
 
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
 vec3 mod289(vec3 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
@@ -30,6 +34,10 @@ vec4 mod289(vec4 x) {
 }
 
 float permute(float x) {
+  return mod289(((x*34.0)+10.0)*x);
+}
+
+vec3 permute(vec3 x) {
   return mod289(((x*34.0)+10.0)*x);
 }
 
@@ -50,19 +58,6 @@ vec4 taylorInvSqrt(vec4 r) {
 
 
 // 2D:
-
-vec3 mod289_2d(vec3 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec2 mod289_2d(vec2 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec3 permute_2d(vec3 x) {
-  return mod289(((x*34.0)+10.0)*x);
-}
-
 
 float snoise(vec2 v) {
   const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
@@ -85,8 +80,8 @@ float snoise(vec2 v) {
   x12.xy -= i1;
 
 // Permutations
-  i = mod289_2d(i); // Avoid truncation effects in permutation
-  vec3 p = permute_2d( permute_2d( i.y + vec3(0.0, i1.y, 1.0 ))
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
 		+ i.x + vec3(0.0, i1.x, 1.0 ));
 
   vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
@@ -189,6 +184,8 @@ float snoise(vec3 v)
                                 dot(p2,x2), dot(p3,x3) ) );
 }
 
+
+
 // 4D:
 
 vec4 grad4(float j, vec4 ip)
@@ -207,27 +204,26 @@ vec4 grad4(float j, vec4 ip)
 // (sqrt(5) - 1)/4 = F4, used once below
 #define F4 0.309016994374947451
 
-float snoise(vec4 v)
-{
+float snoise(vec4 v) {
   const vec4  C = vec4( 0.138196601125011,  // (5 - sqrt(5))/20  G4
                         0.276393202250021,  // 2 * G4
                         0.414589803375032,  // 3 * G4
                        -0.447213595499958); // -1 + 4 * G4
 
-// First corner
+  // First corner
   vec4 i  = floor(v + dot(v, vec4(F4)) );
   vec4 x0 = v -   i + dot(i, C.xxxx);
 
-// Other corners
+  // Other corners
 
-// Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
+  // Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
   vec4 i0;
   vec3 isX = step( x0.yzw, x0.xxx );
   vec3 isYZ = step( x0.zww, x0.yyz );
-//  i0.x = dot( isX, vec3( 1.0 ) );
+  //  i0.x = dot( isX, vec3( 1.0 ) );
   i0.x = isX.x + isX.y + isX.z;
   i0.yzw = 1.0 - isX;
-//  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
+  //  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
   i0.y += isYZ.x + isYZ.y;
   i0.zw += 1.0 - isYZ.xy;
   i0.z += isYZ.z;
@@ -248,7 +244,7 @@ float snoise(vec4 v)
   vec4 x3 = x0 - i3 + C.zzzz;
   vec4 x4 = x0 + C.wwww;
 
-// Permutations
+  // Permutations
   i = mod289(i); 
   float j0 = permute( permute( permute( permute(i.w) + i.z) + i.y) + i.x);
   vec4 j1 = permute( permute( permute( permute (
@@ -257,8 +253,8 @@ float snoise(vec4 v)
            + i.y + vec4(i1.y, i2.y, i3.y, 1.0 ))
            + i.x + vec4(i1.x, i2.x, i3.x, 1.0 ));
 
-// Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
-// 7*7*6 = 294, which is close to the ring size 17*17 = 289.
+  // Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
+  // 7*7*6 = 294, which is close to the ring size 17*17 = 289.
   vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;
 
   vec4 p0 = grad4(j0,   ip);
@@ -267,7 +263,7 @@ float snoise(vec4 v)
   vec4 p3 = grad4(j1.z, ip);
   vec4 p4 = grad4(j1.w, ip);
 
-// Normalise gradients
+  // Normalise gradients
   vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
   p0 *= norm.x;
   p1 *= norm.y;
@@ -275,7 +271,7 @@ float snoise(vec4 v)
   p3 *= norm.w;
   p4 *= taylorInvSqrt(dot(p4,p4));
 
-// Mix contributions from the five corners
+  // Mix contributions from the five corners
   vec3 m0 = max(0.57 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);
   vec2 m1 = max(0.57 - vec2(dot(x3,x3), dot(x4,x4)            ), 0.0);
   m0 = m0 * m0;
@@ -283,7 +279,7 @@ float snoise(vec4 v)
   return 60.1 * ( dot(m0*m0, vec3( dot( p0, x0 ), dot( p1, x1 ), dot( p2, x2 )))
                + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;
 
-  }
+}
 
 
 
@@ -309,4 +305,31 @@ ${generics(['vec2', 'vec3', 'vec4'], /* glsl */`
     return total / maxValue;
   }
 `)}
+
+float snoiseFast4D(vec4 p) {
+  const float SCALAR = 0.01;
+  float a = SCALAR * dot(p.xy * 0.3, vec2(-127.1, 311.7));
+  float b = SCALAR * dot(p.zw * 0.8, vec2(269.5, -183.3));
+  float c = SCALAR * dot(p.wx * 6.0, vec2(23.14069, 2.665157));
+  float d = SCALAR * dot(p.zy * 1.3, vec2(-12.9898, 78.233));
+  return (snoise(vec2(a, b)) + snoise(vec2(d, c))) * 0.5;
+}
+
+float fnoiseFast4D(vec4 p, int octaves, float persistence) {
+  float total = 0.0;           // Final noise value
+  float amplitude = 1.0;       // Initial amplitude
+  float frequency = 1.0;       // Initial frequency
+  float maxValue = 0.0;        // Used for normalization
+
+  for (int i = 0; i < octaves; i++) {
+    total += snoiseFast4D(p * frequency) * amplitude;
+
+    maxValue += amplitude;   // Keep track of max amplitude
+    amplitude *= persistence; // Reduce amplitude for next octave
+    frequency *= 2.0;        // Increase frequency for next octave
+  }
+
+  // Normalize the result to stay within the range [0, 1]
+  return total / maxValue;
+}
 `
