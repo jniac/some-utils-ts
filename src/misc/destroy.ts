be68bb788a38500023b80386e2dd7eb86cb746b5
jniac <joseph.merdrignac@gmail.com>
2025-06-17 09:30:49 +0200

    DestroyableInstance is more powerful (support tree structure)

--- src/misc/destroy.ts
+++ src/misc/destroy.ts
@@ -1,4 +1,4 @@
-import { Destroyable } from '../types'
+import { Destroyable, DestroyableObject } from '../types'
 
 /**
  * Simplifies the destruction of objects and functions.
@@ -18,23 +18,71 @@ export function destroy(...destroyables: Destroyable[] | Destroyable[][]): void 
   }
 }
 
-export class DestroyableInstance {
-  destroyables: Destroyable[] = []
-
-  collect(...values: (Destroyable | null | undefined | Iterable<Destroyable | null | undefined>)[]) {
-    for (const value of values) {
-      if (value) {
-        if (Symbol.iterator in value) {
-          for (const destroyable of value) {
-            if (destroyable) {
-              this.destroyables.push(destroyable)
-            }
+export function flatDestroyables(
+  ...values: (Destroyable | null | undefined | Iterable<Destroyable | null | undefined>)[]
+): Destroyable[] {
+  const result: Destroyable[] = []
+  for (const value of values) {
+    if (value) {
+      if (Symbol.iterator in value) {
+        for (const destroyable of value) {
+          if (destroyable) {
+            result.push(destroyable)
           }
-        } else {
-          this.destroyables.push(value)
         }
+      } else {
+        result.push(value)
       }
     }
+  }
+  return result
+}
+
+export class DestroyableInstance<T = null> {
+  #destroyables = new Set<Destroyable>()
+  #destroyed = false
+
+  customData: T
+
+  get destroyed() {
+    return this.#destroyed
+  }
+
+  constructor(customData?: T) {
+    this.customData = (customData ?? null) as T
+  }
+
+  /**
+   * Bind this instance to a parent instance, allowing it to be collected and 
+   * destroyed when the parent is destroyed.
+   * 
+   * Note:
+   * - ⚠️ This cannot be undone.
+   */
+  bindTo(parentInstance: DestroyableInstance<T>): this {
+    if (this.#destroyed)
+      return this
+
+    parentInstance.collect(this)
+
+    return this
+  }
+
+  collect(...values: (Destroyable | null | undefined | Iterable<Destroyable | null | undefined>)[]): DestroyableObject {
+    if (this.#destroyed)
+      return { destroy: () => { } }
+
+    const safeDestroyables = flatDestroyables(...values)
+
+    for (const destroyable of safeDestroyables)
+      this.#destroyables.add(destroyable)
+
+    const destroy = () => {
+      for (const destroyable of safeDestroyables)
+        this.#destroyables.delete(destroyable)
+    }
+
+    return { destroy }
   }
 
   /**
@@ -43,8 +91,19 @@ export class DestroyableInstance {
    * NOTE: This method is bound to the instance and can be passed as a callback.
    */
   destroy = () => {
-    destroy(this.destroyables)
-    this.destroyables = []
+    if (this.#destroyed)
+      return
+
+    this.#destroyed = true
+
+    for (const destroyable of this.#destroyables) {
+      if (typeof destroyable === 'function') {
+        destroyable()
+      } else if ('destroy' in destroyable) {
+        destroyable.destroy()
+      }
+    }
+    this.#destroyables.clear()
   }
 }
 
