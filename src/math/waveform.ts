e9dc2487772924d8fc6ff76550ae967012590b37
jniac <joseph.merdrignac@gmail.com>
2025-09-06 13:18:27 +0200

    (details) aliases

--- src/math/waveform.ts
+++ src/math/waveform.ts
@@ -1,5 +1,21 @@
-export function triangle(x: number, { frequency = 1, phase = 0 } = {}) {
-  let x1 = x * frequency + phase
+export function triangle(x: number, {
+  frequency = 1,
+  phase = 0,
+  f = frequency,
+  p = phase,
+}: {
+  frequency?: number
+  phase?: number
+  /**
+   * Alias for frequency
+   */
+  f?: number
+  /**
+   * Alias for phase
+   */
+  p?: number
+} = {}) {
+  let x1 = x * f + p
   let y = x1 * 2 - Math.floor(x1 * 2)
   return (
     x1 - Math.floor(x1) < 0.5
