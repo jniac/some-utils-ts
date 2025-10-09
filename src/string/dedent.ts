7cb6fd6e05c42ba0792b384d012d9e6713b99e1f
jniac <joseph.merdrignac@gmail.com>
2025-10-09 22:48:02 +0200

    improve string/dedent

--- src/string/dedent.ts
+++ src/string/dedent.ts
@@ -1,7 +1,19 @@
+
+/**
+ * Removes leading indentation from a multi-line string.
+ *
+ * It detects the minimum indentation level from the *first* non-empty line
+ * and removes that amount of leading whitespace from all lines.
+ */
 export function dedent(str: string): string {
-  const lines = str.replace(/^\n/, '').split('\n')
-  const indent = Math.min(
-    ...lines.filter(l => l.trim()).map(l => l.match(/^(\s*)/)![1].length)
-  )
-  return lines.map(l => l.slice(indent)).join('\n').trim()
+  const lines = str.split('\n')
+  let indent = 0
+  for (const line of lines) {
+    const match = line.match(/^(\s*)\S/)
+    if (match) {
+      indent = match[1].length
+      break
+    }
+  }
+  return lines.map(l => l.slice(Math.min(l.length, indent))).join('\n')
 }
