f42621325ba3f520fbbec7cf4a280edf0af4c89b
jniac <joseph.merdrignac@gmail.com>
2025-10-15 13:55:03 +0200

    iteration/map-with-separator (mapWithSeparator)

---
+++ src/iteration/map-with-separator.ts
@@ -0,0 +1,32 @@
+/**
+ * Maps each item in an iterable to a new value, inserting a separator value between each pair of items.
+ * 
+ * Notes:
+ * - The index passed to the `map` and `separator` functions is the index in the resulting array (max: 2 * items.length - 1).
+ */
+export function mapWithSeparator<T, U>(
+  items: Iterable<T>,
+  map: (item: T, index: number) => U,
+  separator: (info: { index: number, left: T, right: T }) => U,
+): U[] {
+  const result: U[] = []
+  let index = 0
+  let it = items[Symbol.iterator]()
+  let current = it.next()
+  let previous: T | undefined = undefined
+  if (!current.done) {
+    result.push(map(current.value, index))
+    index++
+    previous = current.value
+    current = it.next()
+  }
+  while (!current.done) {
+    result.push(separator({ index, left: previous!, right: current.value }))
+    index++
+    result.push(map(current.value, index))
+    index++
+    previous = current.value
+    current = it.next()
+  }
+  return result
+}
