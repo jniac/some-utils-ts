04db666916e7f09a701843c3ad20b3bf23dee896
jniac <joseph.merdrignac@gmail.com>
2025-12-11 20:14:19 +0100

    dedent "removePaddingEmptyLines" option

--- src/string/dedent.ts
+++ src/string/dedent.ts
@@ -5,9 +5,13 @@
  * It detects the minimum indentation level from the *first* non-empty line
  * and removes that amount of leading whitespace from all lines.
  */
-export function dedent(str: string): string {
+export function dedent(str: string, { removePaddingEmptyLines = true } = {}): string {
   const lines = str.split('\n')
   let indent = 0
+  while (removePaddingEmptyLines && lines.length > 0 && lines[0].trim() === '')
+    lines.shift()
+  while (removePaddingEmptyLines && lines.length > 0 && lines[lines.length - 1].trim() === '')
+    lines.pop()
   for (const line of lines) {
     const match = line.match(/^(\s*)\S/)
     if (match) {
