dc620e0680edb40334b730feee3b49f99178a395
jniac <joseph.merdrignac@gmail.com>
2025-12-17 17:54:23 +0100

    mapWithSeparator: + originalIndex

--- src/iteration/map-with-separator.ts
+++ src/iteration/map-with-separator.ts
@@ -6,16 +6,17 @@
  */
 export function mapWithSeparator<T, U>(
   items: Iterable<T>,
-  map: (item: T, index: number) => U,
+  map: (item: T, index: number, originalIndex: number) => U,
   separator: (info: { index: number, left: T, right: T }) => U,
 ): U[] {
   const result: U[] = []
   let index = 0
+  let originalIndex = 0
   let it = items[Symbol.iterator]()
   let current = it.next()
   let previous: T | undefined = undefined
   if (!current.done) {
-    result.push(map(current.value, index))
+    result.push(map(current.value, index, originalIndex))
     index++
     previous = current.value
     current = it.next()
@@ -23,7 +24,8 @@ export function mapWithSeparator<T, U>(
   while (!current.done) {
     result.push(separator({ index, left: previous!, right: current.value }))
     index++
-    result.push(map(current.value, index))
+    result.push(map(current.value, index, originalIndex))
+    originalIndex++
     index++
     previous = current.value
     current = it.next()
