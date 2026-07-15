9cea8183a81376150bfcd24776cf34f4e5897fd9
jniac <joseph.merdrignac@gmail.com>
2026-07-16 00:56:11 +0200

    dedent can be used as a tag function

--- src/string/dedent.ts
+++ src/string/dedent.ts
@@ -1,11 +1,44 @@
 
+
+const defaultOptions = {
+  removePaddingEmptyLines: true,
+} as const
+
 /**
  * Removes leading indentation from a multi-line string.
  *
- * It detects the minimum indentation level from the *first* non-empty line
- * and removes that amount of leading whitespace from all lines.
+ * Example:
+ * ```ts
+ * const str = dedent`
+ *     👉 NOTICE 👈
+ * 
+ *   This is a multi-line string.
+ *   It has leading indentation that will be removed.
+ * 
+ *   Features:
+ *     - It preserves relative indentation.
+ *     - It preserves empty lines.
+ *     - Deduces the minimum indentation across all lines (not only the first one).
+ *     - It can be used as a template literal tag or as a function.
+ * `
+ * console.log(str)
+ * ```
  */
-export function dedent(str: string, { removePaddingEmptyLines = true } = {}): string {
+export function dedent(strings: TemplateStringsArray, ...values: unknown[]): string
+export function dedent(str: string, options?: Partial<typeof defaultOptions>): string
+export function dedent(...args: any[]): string {
+  let str: string
+  let removePaddingEmptyLines = defaultOptions.removePaddingEmptyLines
+  if (typeof args[0] === 'string') {
+    str = args[0]
+    if (args[1]?.removePaddingEmptyLines !== undefined)
+      removePaddingEmptyLines = args[1].removePaddingEmptyLines
+  } else {
+    const strings: TemplateStringsArray = args[0]
+    const values: unknown[] = args.slice(1)
+    str = strings.reduce((acc, s, i) => acc + s + (i < values.length ? String(values[i]) : ''), '')
+  }
+
   const lines = str.split('\n')
   let indent = 0
   while (removePaddingEmptyLines && lines.length > 0 && lines[0].trim() === '')
@@ -15,8 +48,9 @@ export function dedent(str: string, { removePaddingEmptyLines = true } = {}): st
   for (const line of lines) {
     const match = line.match(/^(\s*)\S/)
     if (match) {
-      indent = match[1].length
-      break
+      const indentNew = match[1].length
+      if (indent === 0 || indentNew < indent)
+        indent = indentNew
     }
   }
   return lines.map(l => l.slice(Math.min(l.length, indent))).join('\n')
