7f5a8ceaff850f326bdd01c11d4afa025ba7b44c
jniac <joseph.merdrignac@gmail.com>
2026-07-30 12:27:39 +0200

    glsl_bend: with normal !

--- src/glsl/transform/bend.ts
+++ src/glsl/transform/bend.ts
@@ -1,20 +1,49 @@
 export const glsl_bend = /* glsl */`
 #ifndef GLSL_BEND
 #define GLSL_BEND
-  vec4 applyBend(vec4 position, float factor, mat4 bendMatrix, mat4 bendMatrixInverse) {
-    float q = 1.0 / factor;
-    
-    if (abs(factor) < 0.0001)
-      return position;
-    
+
+  void applyBend(
+    inout vec4 position,
+    inout vec3 normal,
+    float factor,
+    mat4 bendMatrix,
+    mat4 bendMatrixInverse
+  ) {
+    if (abs(factor) < 0.0001) {
+      return;
+    }
+
     position = bendMatrixInverse * position;
-    vec2 center = vec2(0.0, q);
-    float a = position.x / q;
-    float r = center.y - position.y;
-    position.x = center.x + r * sin(a);
-    position.y = center.y + -r * cos(a);
+    normal = mat3(bendMatrixInverse) * normal;
+
+    float angle = position.x * factor;
+    float radiusFactor = 1.0 - position.y * factor;
+
+    float c = cos(angle);
+    float s = sin(angle);
+
+    float safeRadiusFactor =
+      abs(radiusFactor) < 0.000001
+        ? (radiusFactor < 0.0 ? -0.000001 : 0.000001)
+        : radiusFactor;
+
+    normal = vec3(
+      c * normal.x / safeRadiusFactor - s * normal.y,
+      s * normal.x / safeRadiusFactor + c * normal.y,
+      normal.z
+    );
+
+    float q = 1.0 / factor;
+    float radius = q - position.y;
+
+    position.xy = vec2(
+      radius * s,
+      q - radius * c
+    );
+
     position = bendMatrix * position;
-    return position;
+    normal = normalize(mat3(bendMatrix) * normal);
   }
+  
 #endif
 `
